1edbff2c91ee75a5aa92af5e38a40a9dbbd8303e
Ekusi <andrewrdakers@outlook.com>
2014-07-19 17:16:05 -0700

    Returns score for any English word

--- js/scripts.js
+++ js/scripts.js
@@ -2,60 +2,49 @@
  	$("form#form").submit(function (event) {
  		event.preventDefault();
  		var word = $("#word-input").val();
- 		$("#output").append("<li>" + word + "</li>");
- 	});
+ 		$("#word-input").val('');
+
+
+word = word.toUpperCase();
+var scrabbleScore = {
+	A:1,
+	E:1,
+	I:1,
+	O:1,
+	U:1,
+	L:1,
+	N:1,
+	R:1,
+	S:1,
+	T:1,
+	D:2,
+	G:2,
+	B:3,
+	C:3,
+	M:3,
+	P:3,
+	F:4,
+	H:4,
+	V:4,
+	W:4,
+	Y:4,
+	K:5,
+	J:8,
+	X:8,
+	Q:10,
+	Z:10,
+}
+
+var letter = 0;
+var score = 0;
+for (i = 0; i < word.length; i++) {
+    letter = word[i];
+    score += scrabbleScore[letter];
+}
+
+$("#output").append("<li>" + "The word " + word + " is worth " + score + " points" + "</li>");
+
+
+
+	});
  });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- // var scrabbleScore = {
- // 	oneP: ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
-  // 	twoP: ["D", "G"],
- 	// threeP: ["B", "C", "M", "P"],
- 	// fourP: ["F", "H", "V", "W", "Y"],
- 	// fiveP: ["k"],
- 	// sixP: ["J", "X"],
- 	// sevenP: ["Q", "Z"],
-
-// if ("A" === "A") {
- 	// 	return 1;
- 	// } else {
- 	// 	return false;
- 	// };
